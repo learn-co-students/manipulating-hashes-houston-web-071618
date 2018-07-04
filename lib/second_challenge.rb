412ee0b3a9bac58d27e4f08fb11fe89021ca6f24
kevinptx <kevin.peery@gmail.com>
2018-07-03 22:18:10 -0500

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,14 @@
+require "pry"
+
 def second_challenge
   groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"],
   }
 
   #code your solution here!
-
-  
-
+  #binding.pry
+  groceries.values.flatten
 end
